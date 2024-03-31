fdc006bb58d39f71eda59943d43bfa84b6e0a703
subhomoyganguli <subhomoyganguli@gmail.com>
2024-03-31 13:35:18 +0530

    Two new functions

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,17 @@
 
 print("adding all the data processing code related to fraud detection")
 
+a = 5
+b = 7
+
+def sum(a,b):
+    c = a+b
+    return c
+
+print("sum function")
+
+def multiply(a,b):
+    d = a*b
+    return b
+
+print("multiply function")
