395cf92661ea5a049457e5c450fb28928b331359
subhomoyganguli <subhomoyganguli@gmail.com>
2024-03-31 10:35:19 +0530

    Revert "Function added"
    This reverts commit 790265b3e4f2a96fcae4d3f23e35efa0f6591a9d.
    
    "Changes Reverted"

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,3 @@
 
 print("adding all the data processing code related to fraud detection")
 
-a = 5
-b = 1
-
-def sum(a,b):
-    c = a+b
-    return a
-
-print("added a new function which is incorrect")
