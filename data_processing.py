790265b3e4f2a96fcae4d3f23e35efa0f6591a9d
subhomoyganguli <subhomoyganguli@gmail.com>
2024-03-31 10:33:47 +0530

    Function added

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,11 @@
 
 print("adding all the data processing code related to fraud detection")
 
+a = 5
+b = 1
+
+def sum(a,b):
+    c = a+b
+    return a
+
+print("added a new function which is incorrect")
